8b302629d2916febf75fc1ef9bd73d8af927994a
Vitālijs KOrabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2023-03-06 21:35:50 +0200

    commit

---
+++ js1.js
@@ -0,0 +1,29 @@
+let num_1 = 33; 
+// Integer
+let num_2 = 2.45
+// Double,float
+let text = "<h1>Hello</h1>";
+let text2 = 'lorem \'ipsum\'';
+
+// \ Экранирует символ
+let text3 = `https://www.ss.com/`;
+// String
+
+let checked = true; //false;
+
+// Примитивы
+
+// составные типы
+
+let images = [1,"img1.jpg",true,["img2.html"]];
+
+let home = {
+    roomCount:6,
+    floorCount:3,
+    colors:["yellow","green","black"],
+    family:{
+        father:"John",
+        mother:"Mary"
+    }
+}
+document.write(home.family.father);
